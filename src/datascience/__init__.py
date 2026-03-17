82c6cda469191b0ec3587c6b79b0d98f9cc6dfd4
vinaypurswani53 <vinaypurswani53@gmail.com>
2026-03-17 17:37:14 +0000

    Data Validation Completed

--- src/datascience/__init__.py
+++ src/datascience/__init__.py
@@ -2,20 +2,19 @@ import os
 import sys
 import logging
 
-logging_str="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
-
-log_dir="logs"
-log_filepath=os.path.join(log_dir,"logging.log")
-os.makedirs(log_dir,exist_ok=True)
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "logging.log")
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
-
     handlers=[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
-    ]
+    ],
+    force=True # <--- ADD THIS LINE
 )
 
-logger=logging.getLogger("datasciencelogger")
+logger = logging.getLogger("datascienceLogger")
